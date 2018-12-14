1d4187305daec1c8ff82b2ba348158e037b08154
daynor2018 <keydayp@gmail.com>
2018-12-14 16:22:12 -0430

    ultimopaso

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql10.freesqldatabase.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql10270065'),
+            'username' => env('DB_USERNAME', 'sql10270065'),
+            'password' => env('DB_PASSWORD', 'r9XCc4QyGm'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
